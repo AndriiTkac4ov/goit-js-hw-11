76d672252e55db1b3a9b0872af38221186b5a6ef
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-18 20:41:45 +0000

    Deploying to gh-pages from @ AndriiTkac4ov/goit-js-hw-11@e3c2732ea0ddf1a535f08d4fb014cc85665da99b 🚀

---
+++ index.08bf9c96.js
@@ -0,0 +1,2 @@
+!function(){var e={searchInput:document.querySelector("input"),searchBtn:document.querySelector("button"),searchForm:document.querySelector("#search-form")};e.searchForm.addEventListener("submit",(function(e){e.preventDefault(),console.log("Hello JS!!!")})),console.log(e.searchInput),console.log(e.searchBtn),console.log(e.searchForm)}();
+//# sourceMappingURL=index.08bf9c96.js.map
