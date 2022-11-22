10de3e7b06605c153b24ff4a0c22607f26efe985
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-11-22 21:00:53 +0200

    hide not async api-service

--- src/api-service.js
+++ src/api-service.js
@@ -1,50 +1,50 @@
-import getRefs from './get-refs';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import getRefs from './get-refs';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = getRefs();
+// const refs = getRefs();
 
-const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = '31433732-587fed4cb039ee24c3149a17c';
-const perPage = 40;
+// const BASE_URL = 'https://pixabay.com/api';
+// const API_KEY = '31433732-587fed4cb039ee24c3149a17c';
+// const perPage = 40;
 
-export default class ImagesApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
-    }
+// export default class ImagesApiService {
+//     constructor() {
+//         this.searchQuery = '';
+//         this.page = 1;
+//     }
 
-    fetchImages() {
-        const URL =`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${perPage}`;
+//     fetchImages() {
+//         const URL =`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${perPage}`;
 
-        return fetch(URL)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.status);
-                };
+//         return fetch(URL)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw new Error(response.status);
+//                 };
     
-                return response.json();
-            })
-            .then(data => {
-                this.page += 1;
-                const { totalHits, hits } = data;
-                if (totalHits / (this.page - 1) < perPage && hits.length !== 0) {
-                    refs.loadMoreBtn.classList.add('is-hidden');
-                    Notify.info("We're sorry, but you've reached the end of search results.");
-                };
+//                 return response.json();
+//             })
+//             .then(data => {
+//                 this.page += 1;
+//                 const { totalHits, hits } = data;
+//                 if (totalHits / (this.page - 1) < perPage && hits.length !== 0) {
+//                     refs.loadMoreBtn.classList.add('is-hidden');
+//                     Notify.info("We're sorry, but you've reached the end of search results.");
+//                 };
 
-                return { totalHits, hits };
-            });
-    }
+//                 return { totalHits, hits };
+//             });
+//     }
 
-    resetPage() {
-        this.page = 1;
-    }
+//     resetPage() {
+//         this.page = 1;
+//     }
 
-    get query() {
-        return this.searchQuery;
-    }
+//     get query() {
+//         return this.searchQuery;
+//     }
 
-    set query(newSearchQuery) {
-        return this.searchQuery = newSearchQuery;
-    }
-}
+//     set query(newSearchQuery) {
+//         return this.searchQuery = newSearchQuery;
+//     }
+// }
